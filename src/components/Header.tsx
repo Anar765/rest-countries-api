24049e7185053921dbfaac6115e02421a80e3a52
Anar765 <anar.aliyev181@gmail.com>
2026-02-28 12:15:37 +0400

    feat(ui): Add icon to theme toggle button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import { useContext } from "react"
 import { AppContext } from "../App"
+import { moonOutline, sunnyOutline } from "ionicons/icons"
+import { IonIcon } from "@ionic/react"
 
 const Header = () => {
 
@@ -9,8 +11,9 @@ const Header = () => {
         <header className="w-full py-5 flex items-center justify-center bg-white shadow-sm shadow-Grey-400">
             <div className="w-full max-w-400 flex items-center justify-between">
                 <h1 className="text-3xl font-bold">Where in the world?</h1>
-                <button onClick={() => setIsDarkMode(prevState => !prevState)} className="text-lg">
+                <button onClick={() => setIsDarkMode(prevState => !prevState)} className="text-lg flex items-center gap-3">
                     {/* Icon */}
+                    <IonIcon icon={isDarkMode ? moonOutline : sunnyOutline} className="w-5 h-5" />
                     <p>{isDarkMode ? "Dark" : "Light"} Mode</p>
                 </button>
             </div>
